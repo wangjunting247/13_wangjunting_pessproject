870a71691f5e88253829d23b5e5d9ae69f961102
wangjunting247 <wang_jun_ting@connect.ite.edu.sg>
2021-05-27 13:47:59 +0800

    initial commit

---
+++ nav.php
@@ -0,0 +1,9 @@
+<h1>Police Emergency Service System</h1>
+<nav id="HeaderStyle">
+	<ul>
+	<a href="logcall.php">Log Call</a>
+	<a href="update.php">Update</a>
+	<a href="#">Report</a>
+	<a href="#">History</a>
+	</ul>
+</nav>
